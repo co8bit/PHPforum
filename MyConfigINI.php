d175a30151d99295dcb8b12fde5101321d90534c
co8bit <co8bit@gmail.com>
2013-11-16 02:00:52 +0800

    [接上]做了些页面

---
+++ MyConfigINI.php
@@ -0,0 +1,17 @@
+<?php
+$_SOFTNAME = "基于LBS的可视化论坛";
+$_VERSION = "1.0.0";
+
+$SQLIP="localhost";
+$SQLID="qianbao";
+$SQLPA="85208520+ht";
+$DATA_DB="qianbao";
+
+$_ANNOUNCEMENT = array("本系统正在测试","已升级成为1.0.0版本","空","空","空","空","空");//预留7个空
+/*
+ 权限：
+销售     进货     库存管理     应收款     应付款     员工管理   财务  账号 
+0     1    2     3    4      5    6  7
+*/
+$_POWERCHINESE = array("销售","进货","库存","应收款","应付款","员工管理","财务","账号");
+?>
